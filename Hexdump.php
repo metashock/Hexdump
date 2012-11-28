16f73661b0f8ce04d4554ff03ba49629c318cda1
Thorsten Heymann <info@metashock.net>
2012-11-28 14:42:34 +0100

    Made the code comptatible to the PEAR coding standards
    Used PHP Code Sniffer to detect incompatibilities to the PEAR coding
    standards.
    
    Extended code documentation

--- Hexdump.php
+++ Hexdump.php
@@ -1,106 +1,110 @@
 <?php
-/*
-
-    Copyright (c) 2011, Thorsten Heymann
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-
-    - Redistributions of source code must retain the above copyright 
-      notice, this list of conditions and the following disclaimer.
-
-    - Redistributions in binary form must reproduce the above copyright 
-      notice, this list of conditions and the following disclaimer in the 
-      documentation and/or other materials provided with the distribution.
-
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-    POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
 /**
- *  This library contains the hexdump function
- *  
- *      - hexdump()     prints a hexdump of data. you can configure the output.
- * 
- *  @author thorsten heymann 
- *  @copyright thorsten heymann
- */
-/**
- *  prints a hexdump of data. you can configure the output.
+ * This library contains a hexdump function.
  *
- *  @param string $data
- *  @param integer $ncolumns = 16
- *  @param string $linedelim = PHP_EOL
- *  @param boolean $uppercase = FALSE
+ * PHP Version 4.0.0
+ * 
+ * @category  Debug
+ * @package   Hexdump
+ * @author    Thorsten Heymann <info@metashock.net>
+ * @copyright 2011 - 2012 Thorsten Heymann
+ * @license   http://www.opensource.org/licenses/bsd-license.php BSD
+ * @version   0.1.1
+ * @link      http://www.metashock.de/pirum
+ * @since     0.1.0
+ */
+
+/**
+ * Prints a hexdump of data. You can configure the number of
+ * bytes per line, the linedelimiter and choose whether hexadecimal
+ * numbers should be uppercased or not.
+ *
+ * @param string  $data      The data to be hexdumped 
+ * @param integer $ncolumns  The number of bytes per line 
+ * @param string  $linedelim The line delimiter
+ * @param boolean $uppercase If true hexadecimal number will be uppercased
+ *
+ * @return void
+ *
+ * @throws InvalidArgumentException if $data is not a string
+ *
+ * @version 0.1.1
+ * @since 0.1.0
  */
 function hexdump (
     $data,
     $ncolumns = 16,
     $linedelim = PHP_EOL,
-    $uppercase = FALSE
+    $uppercase = false
 ) {
-    ob_start(NULL, 4096);
 
-    if (is_string($data)){
-        $len = strlen($data);
-    } else if (is_array($data)) {
-        $len = count($data);
-    } else {
-        throw new InvalidArgumentException (
-            '$data expected to be array or string. ' . gettype($data) . ' found.');
-    }
-
-    // using translation table for ascii output
-    // @thanks to mindplay.dk for the strtr() idea
-    //
-    // Formerly I translated char by char to ascii. Now I'm using a static 
-    // translation buffer and translate $ncolumns chars at once 
-    // delegating the inner work to strtr(). In my tests this was slight faster.
-    // See http://stackoverflow.com/questions/1057572/how-can-i-get-a-hex-dump-of-a-string-in-php
-    // for more details
+    // will contain a binary string with all non printable bytes
     static $from = '';
+    // will contain a string containing length of $from times '.'
     static $to = '';
+
+    // the column of the current byte
+    $c = 1;
+    // total offset in $data
+    $offset = 0;
+
+    // check argument type grab length of $data
+    if (is_string($data)) {
+        $len = strlen($data);
+    } else {
+        throw new InvalidArgumentException(
+            '$data expected to be string. ' . gettype($data) . ' found.'
+        );
+    }
+
+    // using output buffering to increase performance
+    ob_start(null, 4096);
+
+    // prepare a translation table to convert non printable bytes
+    // to a '.' char. the translation table will be created statically 
+    // the first time hexdump is called. Using a translation table with 
+    // php's strtr() function appeared slight faster then translating them
+    // in this script directly. @thanks to mindplay.dk for the strtr() idea
+    // @see http://stackoverflow.com/questions/1057572/\ ...
+    // ... how-can-i-get-a-hex-dump-of-a-string-in-php
     if ($from === '') {
-        for ($i=0; $i<0x21; $i++) {
+        for ($i = 0; $i < 0x21; $i++) {
             $from .= chr($i);
             $to .= '.';
         }
 
-        for ($i=0x7E; $i<=0xFF; $i++) {
+        for ($i = 0x7E; $i <= 0xFF; $i++) {
             $from .= chr($i);
             $to .= '.';
         }
     }
+
     
-    $c = 1;
-    $offset = 0;
+    // iterate through $data
     echo '00000000: ';
-    for($i = 0; $i < $len; $i++, $c++){
-        if($uppercase) {
+    for ($i = 0; $i < $len; $i++, $c++) {
+
+        // get byte at current position and convert it to a hex string
+        if ($uppercase) {
             echo strtoupper(bin2hex($data[$i])) . ' ';
         } else {
             echo bin2hex($data[$i]) . ' ';
         }
-        // printf a new line after n * 2 chars
-        if($c === $ncolumns) {
-            // after each $ncolumns chars we print the ascii dump and newline
-            echo '|' , strtr(substr($data, $i - $ncolumns + 1, $ncolumns),
-                $from, $to) , '|' , $linedelim;
+
+        // after the number of bytes has been reached we print
+        // the ascii representation of the last line.
+        if ($c === $ncolumns) {
+            echo '|' ,
+            // non printable characters have to be converted to '.'
+            strtr(substr($data, $i - $ncolumns + 1, $ncolumns), $from, $to) ,
+            '|' , $linedelim; // end of line
+
+            // next line
             $c = 0;
+
+            // increment line offset and prepend it to the new line
             $offset += $ncolumns;
-            if($uppercase) {
+            if ($uppercase) {
                 printf('%08X: ', $offset);
             } else {
                 printf('%08x: ', $offset);
@@ -108,14 +112,18 @@ function hexdump (
         }
     }
 
-    // the last line needs special attention
+    // the last line needs special attention because it may not contain
+    // exactly $ncolumn bytes. the remaining gap between the last hex char 
+    // and the ascii output has therefore to be filled with spaces
+
+    // get the number of remaining bytes
     $remains = $ncolumns - ($i % $ncolumns);
 
-    // display whitespaces for each 'missing' byte
+    // display whitespaces for each remaining byte
     echo str_repeat('   ', $remains)
-    // and the asciis for the last bytes
+    // display the asciis for the last bytes
         , '|' , strtr(substr($data, $i - ($i % $ncolumns)), $from, $to) , '|'
-    // and newline
+    // and a final newline
         , $linedelim;
 
     ob_end_flush();
